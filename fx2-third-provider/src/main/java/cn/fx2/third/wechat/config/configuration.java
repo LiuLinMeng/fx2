3e8b079c709347488449a34be2e774a43625eb52
Administrator <839528187@qq.com>
2020-09-22 21:58:05 +0800

    第三方微信配置文件

---
+++ fx2-third-provider/src/main/java/cn/fx2/third/wechat/config/configuration.java
@@ -0,0 +1,7 @@
+package cn.fx2.third.wechat.config;
+
+import java.io.Serializable;
+
+public class configuration implements Serializable {
+    private static final long serialVersionUID = 6878592846500375742L;
+}
