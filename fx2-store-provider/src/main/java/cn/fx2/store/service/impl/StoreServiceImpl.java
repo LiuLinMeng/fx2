a11a5502fcf8fb8c870a43dcba4a55316347e5d1
Administrator <839528187@qq.com>
2020-09-22 21:33:09 +0800

    定义dubbo超时时间和版本

--- fx2-store-provider/src/main/java/cn/fx2/store/service/impl/StoreServiceImpl.java
+++ fx2-store-provider/src/main/java/cn/fx2/store/service/impl/StoreServiceImpl.java
@@ -3,7 +3,7 @@ package cn.fx2.store.service.impl;
 import cn.fx2.api.service.StoreService;
 import com.alibaba.dubbo.config.annotation.Service;
 
-@Service
+@Service(timeout = 5000,version="1.0.0",interfaceClass = StoreService.class)
 public class StoreServiceImpl implements StoreService {
     @Override
     public String list() {
