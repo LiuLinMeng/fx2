2b90598a6498d3a5ff156814fbcbfdb91f6f99a7
Administrator <839528187@qq.com>
2020-09-28 21:03:16 +0800

    完善登录基本逻辑

--- fx2-mall-customer/src/main/java/cn/fx2/mall/controller/AdminController.java
+++ fx2-mall-customer/src/main/java/cn/fx2/mall/controller/AdminController.java
@@ -3,9 +3,9 @@ package cn.fx2.mall.controller;
 import cn.fx2.api.service.account.AdminService;
 import cn.fx2.common.form.account.LoginForm;
 import cn.fx2.common.utils.JsonResponseUtils;
-import cn.fx2.common.utils.JsonUtils;
 import cn.fx2.common.utils.StatusCodeUtils;
 import com.alibaba.dubbo.config.annotation.Reference;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.validation.BindException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,12 @@ public class AdminController {
 
     @RequestMapping(value = "/admin/login",method = RequestMethod.POST)
     public JsonResponseUtils login(@Validated LoginForm loginForm) throws BindException {
-        System.out.println(JsonUtils.objectToJson(loginForm));
+        if(StringUtils.isBlank(loginForm.getUsername())) {
+            return JsonResponseUtils.error(StatusCodeUtils.PARAMS_NULL.getCode(),"用户名不能为空");
+        }
+        if(StringUtils.isBlank(loginForm.getPassword())) {
+            return JsonResponseUtils.error(StatusCodeUtils.PARAMS_NULL.getCode(),"密码不能为空");
+        }
         try {
             return adminService.login(loginForm.getUsername(),loginForm.getPassword());
         } catch (Exception e) {
